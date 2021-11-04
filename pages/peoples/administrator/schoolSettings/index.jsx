0857bca150b695ec46580971e13b6f588b2240a0
Werfmon <d.vyroubal.w@gmail.com>
2021-11-04 17:08:50 +0100

    access to timetable

--- pages/peoples/administrator/schoolSettings/index.jsx
+++ pages/peoples/administrator/schoolSettings/index.jsx
@@ -5,6 +5,8 @@ import NavBar from '../../../../Components/NavBar'
 import { ThemeProvider } from 'styled-components'
 import { Context } from '../../../_app'
 import { MainHeading, Main} from '../../../../theme'
+import TimetableEnd from '../../../../Components/ComponentsAdministrator/TimetableEnd'
+
 
 
 const Index = () => {
@@ -15,7 +17,7 @@ const Index = () => {
                 <NavBar route="administrator" />
                 <MainHeading>Nastavení školy</MainHeading>
                 <Main>
-
+                    <TimetableEnd />
                 </Main>
             </ThemeProvider>
         </>
