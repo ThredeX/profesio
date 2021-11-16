955b23542124c2d4d71526df6d5a5d111a412a58
Werfmon <d.vyroubal.w@gmail.com>
2021-11-16 20:13:55 +0100

    colors

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,28 +5,53 @@ import { createContext, useEffect } from 'react'
 export const Context = createContext(null)
 
 function MyApp({ Component, pageProps }) {
+	// colors to switch
+	const ORANGE = '#ff5900'
+	const BLUE = '#03d3fc'
+	const RED = '#ff0000'
+	const WHITE = '#ffffff'
+	const GREEN = '#1aff00'
+	const PURPLE = '#5100ff'
+
+	// Background
+	const BG_LIGHT = '#fff'
+	const BG_DARK = '#090909'
+
+	// Navigation
+	const NAV_LIGHT = '#fff'
+	const NAV_DARK = '#141414'
+
+	// text
+	const TEXT_LIGHT = '#000'
+	const TEXT_DARK = '#fff'
+
+	// box
+	const BOX_LIGHT = '#f1f1f1'
+	const BOX_DARK = '#141414'
+
 	let theme = {}
-	if (true) { //dark
+
+	if (true) {
 		theme = {
-			bg: process.env.NEXT_PUBLIC_COLOR_BG_DARK,
-			text: process.env.NEXT_PUBLIC_COLOR_TEXT_DARK,
-			color: process.env.NEXT_PUBLIC_COLOR_RED,
-			nav: process.env.NEXT_PUBLIC_COLOR_NAV_DARK,
-			box: process.env.NEXT_PUBLIC_COLOR_BOX_DARK
+			bg: BG_DARK,
+			text: TEXT_DARK,
+			color: RED,
+			nav: NAV_DARK,
+			box: BOX_DARK,
 		}
-	} else { //light
+	} else {
 		theme = {
-			bg: process.env.NEXT_PUBLIC_COLOR_BG_LIGHT,
-			text: process.env.NEXT_PUBLIC_COLOR_TEXT_LIGHT,
-			color: process.env.NEXT_PUBLIC_COLOR_PURPLE,
-			nav: process.env.NEXT_PUBLIC_COLOR_NAV_LIGHT,
-			box: process.env.NEXT_PUBLIC_COLOR_BOX_LIGHT
+			bg: BG_LIGHT,
+			text: TEXT_LIGHT,
+			color: PURPLE,
+			nav: NAV_LIGHT,
+			box: BOX_LIGHT,
 		}
 	}
-	useEffect(()=> {
+	useEffect(() => {
 		document.body.style.backgroundColor = `${theme.bg}`
 	})
-	
+
 	return (
 		<Context.Provider value={theme}>
 			<Component {...pageProps} />
