fd68cdd3c2c3c62e5bfea3326da8ed2e06c83df5
Werfmon <d.vyroubal.w@gmail.com>
2021-12-11 20:02:28 +0100

    subjectmodify connected to api

--- pages/peoples/administrator/subjectmodify.jsx
+++ pages/peoples/administrator/subjectmodify.jsx
@@ -28,6 +28,9 @@ const Div = styled.div`
 	margin-top: 0.7rem;
 	width: 10rem;
 `
+const Container2 = styled.div`
+	display: flex;
+`
 const Container = styled.div`
 	display: flex;
 	flex-wrap: wrap;
@@ -52,13 +55,13 @@ const Paragraph = styled.p`
 const InputContainer = styled.div`
 	display: flex;
 	align-items: center;
+	flex-direction: column;
 `
 const UnsortedList = styled.ul`
 	height: 18rem;
 	display: flex;
 	flex-direction: column;
 	align-items: space-evenly;
-	justify-content: center;
 	padding-right: 2rem;
 	overflow-y: scroll;
 
@@ -82,52 +85,46 @@ const SubjectModify = () => {
 
 	function deleteSubject(id) {
 		if (confirm('Opravdu chcete odstranit předmět?')) {
-			// fetch(`../../../api/school/subject/${id}`, {
-			//     method: 'DELETE',
-			//     headers: {
-			//         'content-type': 'application/json'
-			//     }
-			// })
-			// .then(res => {
-			//     if(!res.ok){
-			//         console.log(res.status);
-			//     }
-			// })
+			fetch(`../../../api/school/subject/${id}`, {
+				method: 'DELETE',
+				headers: {
+					'content-type': 'application/json',
+				},
+			}).then(res => {
+				if (!res.ok) {
+					console.log(res.status)
+				}
+			})
 			setReload(id)
 		}
 	}
-    function addSubject(e) {
-        	fetch('../../../api/school/subject/', {
-			    method: 'POST',
-			    headers: {
-			        'content-type': 'application/json'
-			    },
-                body: JSON.stringify(e.target.subject.value)
-			})
-			.then(res => {
-			    if(!res.ok){
-			        console.log(res.status);
-			    }
-			})
-    }   
+	function addSubject(e) {
+		e.preventDefault()
+		fetch('../../../api/school/subject', {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+			},
+			body: JSON.stringify({
+				name: e.target.subject.value,
+				short: e.target.subjectShort.value,
+			}),
+		}).then(res => {
+			if (!res.ok) {
+				console.log(res.status)
+			} else setReload(0)
+		})
+	}
 
 	useEffect(() => {
-		setSubjects([
-			{ id: 1, name: 'Linearni algebra I', shortName: 'La' },
-			{ id: 2, name: 'Matematika I', shortName: 'Mat' },
-			{ id: 3, name: 'Linearni algebra II', shortName: 'LaII' },
-			{ id: 4, name: 'programovani I', shortName: 'Prg' },
-			{ id: 5, name: 'programovani II', shortName: 'PrgII' },
-			{ id: 6, name: 'fyzika', shortName: 'Fyz' },
-		])
-        // fetch('../../../api/school/subject')
-        // .then(res => res.json())
-        // .then(data => setSubjects(data))
-        // .catch(err => {
-        //     console.log(err);
-        // })
-	}, [])
-
+		fetch('../../../api/school/subject')
+			.then(res => res.json())
+			.then(data => setSubjects(data))
+			.catch(err => {
+				console.log(err)
+			})
+	}, [reload])
+	console.log(subjects)
 	return (
 		<>
 			<ThemeProvider theme={useContext(Context)}>
@@ -136,30 +133,40 @@ const SubjectModify = () => {
 				<MainHeading>Předměty</MainHeading>
 				<Main>
 					<Container>
-						<Box>
-							<Heading2>Odebrání předmětu</Heading2>
-							<UnsortedList>
-								{subjects?.map(subject => (
-									<List key={subject.id}>
-										<Paragraph>{subject.name}</Paragraph>
-										<Paragraph>{subject.shortName}</Paragraph>
-										<Div>
-											<SubmitButton
-												value="Odstranit"
-												type="submit"
-												onClick={() => deleteSubject(subject.id)}
-											/>
-										</Div>
-									</List>
-								))}
-							</UnsortedList>
-						</Box>
+						{!subjects || subjects[0] ? (
+							<Box>
+								<Heading2>Odebrání předmětu</Heading2>
+								<UnsortedList>
+									{subjects?.map(subject => (
+										<List key={subject.id}>
+											<Paragraph>{subject.name}</Paragraph>
+											<Paragraph>{subject.short}</Paragraph>
+											<Div>
+												<SubmitButton
+													value="Odstranit"
+													type="submit"
+													onClick={() =>
+														deleteSubject(subject.id)
+													}
+												/>
+											</Div>
+										</List>
+									))}
+								</UnsortedList>
+							</Box>
+						) : null}
 						<Box>
 							<Heading2>Přidání předmětu</Heading2>
 							<Form onSubmit={addSubject}>
 								<InputContainer>
-									<Label htmlFor='subject'>Přidat:</Label>
-									<Input name='subject' id='subject'/>
+									<Container2>
+										<Label htmlFor="subject">Předmět:</Label>
+										<Input name="subject" id="subject" />
+									</Container2>
+									<Container2>
+										<Label htmlFor="subjectShort">Zkratka:</Label>
+										<Input name="subjectShort" id="subjectShort" />
+									</Container2>
 								</InputContainer>
 								<SubmitButton type="submit" value="Přidat" />
 							</Form>
