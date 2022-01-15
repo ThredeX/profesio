664c26262a0e775f107a3ad2c349d976c2eec867
Jakub Kropáček <kropikuba@gmail.com>
2022-01-15 19:22:14 +0100

    Added room endpoints, edited perms

--- api/faculty.js
+++ api/faculty.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-const { Faculty, Lecture } = require('../models/')
+const { Faculty, Lecture, Room } = require('../models/')
 const UserChecker = require('../utils/userChecker.js')
 
 // Returns all faculties
@@ -24,6 +24,25 @@ router.delete('/:id', async (req, res) => {
 	const faculty = await Faculty.findByPk(req.params.id)
 	await faculty.destroy()
 	res.status(204).json({ message: 'Faculty deleted' })
+})
+
+router.get('/room', async (req, res) => {
+	if (!UserChecker.doesExist(req.session.user)) return res.status(401).send()
+	const rooms = await Room.findAll()
+	res.status(204).json(rooms.map(r => r.toJSON()))
+})
+
+router.post('/room', async (req, res) => {
+	if (!UserChecker.canEdit(req.session.user)) return res.status(401).send()
+	await Room.create(req.body)
+	res.status(204).json({ message: 'Room created' })
+})
+
+router.delete('/room/:id', async (req, res) => {
+	if (!UserChecker.canEdit(req.session.user)) return res.status(401).send()
+	const room = Room.findByPk(req.params.id)
+	await room.destroy()
+	res.status(204).json({ message: 'Room deleted' })
 })
 
 router.get('/room/:id', async (req, res) => {
