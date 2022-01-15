3909b8a15bf4f7b0105e4629717c68f6ed805b87
Jakub Kropáček <kropikuba@gmail.com>
2022-01-15 23:20:36 +0100

    Parsing data so they can be put into the DB

--- utils/lectureParser.js
+++ utils/lectureParser.js
@@ -1,3 +1,6 @@
+const { Lecture } = require('../models')
+const days = ['mo', 'tu', 'we', 'th', 'fr']
+
 function getTimes(data) {
 	return [
 		data.map(lec => lec.beginning).sort((a, b) => a - b),
@@ -10,10 +13,14 @@ function secToHHMM(seconds) {
 	return `${hours}:${minutes}`
 }
 
+function HHMMtoSec(time) {
+	const [hours, minutes] = time.split(':').map(Number)
+	return hours * 3600 + minutes * 60
+}
+
 function fromDB(data) {
 	const [start, end] = getTimes(data)
-	const ret = {}
-	const days = ['mo', 'tu', 'we', 'th', 'fr']
+	let ret = { subjects: [] }
 	ret['time'] = {
 		start: start.map(time => secToHHMM(time)),
 		end: end.map(time => secToHHMM(time)),
@@ -27,10 +34,10 @@ function fromDB(data) {
 					data[z].beginning === start[j] &&
 					data[z].end === end[j]
 				) {
-					ret[i] = data[z].subject
+					ret['subjects'][i][j] = data[z].subject
 					break
 				} else {
-					ret[i] = [null]
+					ret['subjects'][i][j] = [null]
 				}
 			}
 		}
@@ -38,8 +45,20 @@ function fromDB(data) {
 
 	return ret
 }
-function toDB(data) {
-	return data
+async function toDB(data) {
+	for (let i = 0; i < 5; i++) {
+		for (let j = 0; j < data.subjects[i].length; j++) {
+			await Lecture.create({
+				days: days[i],
+				beginning: HHMMtoSec(data.time.start[j]),
+				end: HHMMtoSec(data.time.end[j]),
+				RoomId: data.subjects[i][j].roomId,
+				TeacherId: data.subjects[i][j].teacherId,
+				SubjetId: data.subjects[i][j].subjectId,
+				FacultyId: data.subjects[i][j].facultyId,
+			})
+		}
+	}
 }
 
 module.exports = { toDB, fromDB }
