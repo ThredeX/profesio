4c6791f7f3f5919950abd7314c41a0c6052e4cea
Werfmon <d.vyroubal.w@gmail.com>
2022-01-20 22:26:13 +0100

    fix

--- Components/ComponentsStudent/ChangeWindow.jsx
+++
@@ -1,8 +0,0 @@
-//pop up window for changing timetable
-export default function ChangeWindow() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
--- Components/ComponentsTeacher/ChangeWindow.jsx
+++
@@ -1,8 +0,0 @@
-//pop up window for cahnging teacher timetable
-export default function ChangeWindow() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
