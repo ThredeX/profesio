1ba25fc3dd5792da85bc5569d9ed300c62c9e005
Werfmon <d.vyroubal.w@gmail.com>
2022-01-26 11:01:09 +0100

    password change setup, lecture log setup

--- Components/PasswordChange.jsx
+++ Components/PasswordChange.jsx
@@ -3,24 +3,37 @@ import { Input, Box, Label, SubmitButton, Heading2 } from '../theme'
 import styled from 'styled-components'
 
 const Div2 = styled.div`
-	width: clamp(20rem, 80vw, 40rem);
+	width: clamp(20rem, 70vw, 30rem);
 `
 const Form = styled.form`
 	width: 100%;
 `
+function changePassword(e) {
+	e.preventDefault()
+	if(e.target.password_1.value !== e.target.password_2.value && e.target.password_1.value == null){
+		return;
+	}
+	fetch('../../api/auth/password', {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json'
+		},
+		body: JSON.stringify({password: e.target.password_1.value})
+	})
+	.then(res => console.log(res))
+	.catch(err => console.error(err))
+}
 
 export default function PasswordChange() {
 	return (
 		<Div2>
 			<Box>
 				<Heading2>Změna hesla</Heading2>
-				<Form>
-					<Label>Zadejte aktualní heslo: </Label>
-					<Input type='password' name='password-now'/>
+				<Form onSubmit={e => changePassword(e)}>
 					<Label>Zadejte nové heslo: </Label>
-					<Input type='password' name='password-new-1'/>
-					<Label>Zadejte nové heslo ještě jednou: </Label>
-					<Input type='password' name='password-new-2'/>
+					<Input type='password' name='password_1'/>
+					<Label>Zadejte nové heslo: </Label>
+					<Input type='password' name='password_2'/>
 					<SubmitButton type="submit" value="Změnit" />
 				</Form>
 			</Box>
