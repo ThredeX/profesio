1e1a5a5f9d8c84cd80f565bbeac59f47d3865613
Werfmon <d.vyroubal.w@gmail.com>
2022-02-03 20:39:21 +0100

    db fix

--- api/school.js
+++ api/school.js
@@ -86,7 +86,7 @@ router.get('/lecture', async (req, res) => {
 router.post('/lecture/:id', async (req, res) => {
 	if (!UserChecker.isStudent(req.session.user)) return res.status(401).send()
 	console.log(req.session.user.student.id, parseInt(req.params.id));
-	await Participation.create(req.session.user.student.id, parseInt(req.params.id))
+	await Participation.create({StudentId: req.session.user.student.id, LectureId: parseInt(req.params.id)})
 	res.status(200)
 })
 
